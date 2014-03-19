706277b684e102473331527e2a0acb3ddcdf4153
Brian Dunnette <brian@dunnette.us>
2014-03-18 20:09:06 -0500

    filtering rows in timesheet view

--- app/scripts/controllers/volunteer-timesheets.js
+++ app/scripts/controllers/volunteer-timesheets.js
@@ -1,16 +1,19 @@
 'use strict';
 
 angular.module('conserjeApp')
-  .controller('VolunteerTimesheetsCtrl', function ($scope, $resource) {
+  .controller('VolunteerTimesheetsCtrl', function ($scope, $resource, $filter, $routeParams) {
     var Timesheet = $resource('http://localhost:3000/timesheets');
     
     var timesheets = Timesheet.query(function () {
         $scope.timesheets = timesheets;
     });
     
+    $scope.volunteerId = $routeParams.volunteerId;
+    
     $scope.gridOptions = { 
       data: 'timesheets',
+      filterOptions: { filterText: 'employee_id:' + $scope.volunteerId },
       sortInfo: { fields: ['date_from'], directions: ['desc']},
-      columnDefs: [{field:'date_from', displayName:'Date'}, {field:'total_attendance|number:2', displayName:'Hours Worked'}]
+      columnDefs: [{field:'date_from', displayName:'Date'}, {field:'total_attendance|number:2', displayName:'Hours Worked'}, {field:'employee_id', displayName:'id'}]
     };
   });
