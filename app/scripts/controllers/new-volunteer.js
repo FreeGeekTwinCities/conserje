ff460dc7c780aa7b8381bb05602ba2db7cf457f7
Brian Dunnette <dunn0172@umn.edu>
2014-03-24 11:33:01 -0500

    replacing img.src with ng-src

--- app/scripts/controllers/new-volunteer.js
+++ app/scripts/controllers/new-volunteer.js
@@ -9,13 +9,14 @@ angular.module('conserjeApp')
     var volunteers = Volunteer.query(function () {
         $scope.volunteers = volunteers;
     });
-      
+    
     $scope.processForm = function () {
         var newVolunteer = new Volunteer($scope.formData);
-        newVolunteer.$save(function(newVolunteer, data) {
+        newVolunteer.$save(function(newVolunteer, company, data) {
           console.log(newVolunteer);
+          console.log($rootScope.company);
           $scope.formData = {};
-          $scope.alerts.push({type: 'success', msg: "Welcome to Free Geek, " + newVolunteer.name + "! You are volunteer #" + newVolunteer.id + "."});
+          $scope.alerts.push({type: 'success', msg: "Welcome to " + $rootScope.company.name + ", " + newVolunteer.name + "! You are volunteer #" + newVolunteer.id + "."});
         });
         
     };
