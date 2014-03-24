fe2d5814e78bc5c1fe4c335a2c8fc8c0f1b8f51c
Brian Dunnette <dunn0172@umn.edu>
2014-03-24 11:56:30 -0500

    adding volunteer info to timesheet display

--- app/scripts/controllers/volunteer-timesheets.js
+++ app/scripts/controllers/volunteer-timesheets.js
@@ -2,13 +2,22 @@
 
 angular.module('conserjeApp')
   .controller('VolunteerTimesheetsCtrl', function ($scope, $resource, $filter, $routeParams, $rootScope) {
+    $scope.volunteerId = $routeParams.volunteerId;
+      
+    var Volunteer = $resource('http://localhost:3000/employees/:volunteerId', {}, { 'get': {method:'GET', isArray: true}});
+    
+    var volunteer = Volunteer.get({volunteerId:$scope.volunteerId}, function () {
+        $scope.volunteer = volunteer[0];
+        console.log($scope.volunteer);
+    });
+      
+    console.log($scope.volunteer);
+    
     var Timesheet = $resource('http://localhost:3000/timesheets');
     
     var timesheets = Timesheet.query(function () {
         $scope.timesheets = timesheets;
     });
-    
-    $scope.volunteerId = $routeParams.volunteerId;
     
     $scope.gridOptions = { 
       data: 'timesheets',
