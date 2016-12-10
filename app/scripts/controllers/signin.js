30e3e6ba7abd86a0c600b7285daa2d53225f38d3
tom hagman <tom.hagman.mac@gmail.com>
2016-12-10 15:08:24 -0600

    added home button, added required action

--- app/scripts/controllers/signin.js
+++ app/scripts/controllers/signin.js
@@ -13,24 +13,34 @@ function SignInController($scope, $rootScope, $resource, $timeout, $modal, Volun
   });
 
   $scope.processForm = function () {
-    var SignIn = $resource('http://' + config.api.host + ':' + config.api.port + '/employees/sign_in');
-    var newSignIn = new SignIn($scope.formData);
-    newSignIn.$save(function(newSignIn, data) {
-        $scope.formData = {};
-        var volunteers = Volunteer.query(function () {
-          $scope.volunteers = volunteers;
-        });
-        var modalInstance = $modal.open({
-          size       : 'lg',
-          template   : "<div class='modal-header'><h1>You're In!</h1></div>" +
-                       "<div class='modal-body'>Time to <h2>Geek Out</h2></div>",
-          controller : ['$scope', '$modalInstance', function($scope, $modalInstance) {}],
-        });
+    if ($scope.formData.employeeId && $scope.formData.departmentId &&
+        $scope.formData.employeeId.length > 0 && $scope.formData.departmentId.length > 0) {
+          var SignIn = $resource('http://' + config.api.host + ':' + config.api.port + '/employees/sign_in');
+          var newSignIn = new SignIn($scope.formData);
+          newSignIn.$save(function(newSignIn, data) {
+              $scope.formData = {};
+              var volunteers = Volunteer.query(function () {
+                $scope.volunteers = volunteers;
+              });
+              var modalInstance = $modal.open({
+                size       : 'lg',
+                template   : "<div class='modal-header'><h1>You're In!</h1></div>" +
+                             "<div class='modal-body'>Time to <h2>Geek Out</h2></div>",
+                controller : ['$scope', '$modalInstance', function($scope, $modalInstance) {}],
+              });
 
-        $timeout(function() {
-          modalInstance.close();
-          $rootScope.go("/")
-        }, 2500);
-    });
+              $timeout(function() {
+                modalInstance.close();
+                $rootScope.go("/")
+              }, 2500);
+          });
+        } else {
+          var modalInstance = $modal.open({
+            size       : 'lg',
+            template   : "<div class='modal-header'>Not Signed in!</div>" +
+                         "<div class='modal-body'><h1>Choose your name and task to sign in!</h1></div>",
+            controller : ['$scope', '$modalInstance', function($scope, $modalInstance) {}],
+          });
+        }
   };
 }
